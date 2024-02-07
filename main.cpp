76df235199a1f58b0a81cbca08b9365e3818cb71
nahumnahum <641724ab@gmail.com>
2024-02-07 11:33:49 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+double intToDouble(const int& i) {
+    double d = static_cast<double>(i);
+    return d;
+}
+
+int main()
+{
+    std::cout << "welcome to my progrum!" << std::endl;
+    int i = 5;
+    double d = intToDouble(i);
+    std::cout << "the size of d is: " << sizeof(d) << std::endl;
+    return 0;
+}
